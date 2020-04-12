f1d5f6c86c906cf7283fde04533cb8662d975c4f
jinxinghong <1096227957@qq.com>
2020-04-12 23:46:17 +0800

    我的踢馆

---
+++ app/activity/king/kicking.js
@@ -0,0 +1,37 @@
+var pageData = new Vue({
+    el: '#page',
+    data: {
+        num: 10,
+        isMe: true,
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+})
+
+
+
+$('body').css('visibility', 'visible');
+$(function () {
+    scrollTop = 0;
+    var isloading = false;
+    var windowHeight = $(window).height();
+    $(window).scroll(function () {
+        var scrollTop = $(this).scrollTop();
+        var scrollHeight = $(document).height();
+        if (scrollTop + windowHeight + 10 >= scrollHeight) {
+            // 限制一秒内只加载一次
+            if (!isloading) {
+                console.log('加载');
+                setTimeout(function () {
+                    pageData._data.num += 10;
+                    isloading = false;
+                }, 1000)
+            }
+            isloading = true;
+        }
+    });
+});
