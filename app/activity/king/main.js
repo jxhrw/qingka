eb47e633ec5eadce1f1668a7ab92f1b532ddc566
jinxinghong <1096227957@qq.com>
2020-04-16 22:34:15 +0800

    接口调试

--- app/activity/king/main.js
+++ app/activity/king/main.js
@@ -2,6 +2,7 @@ var pageData = new Vue({
     el: '#page',
     data: {
         vshow: false,
+        activeTab: 'cs',
     },
     methods: {
         changeAudioUrl: function (audioUrl, index) {
@@ -12,36 +13,23 @@ var pageData = new Vue({
             audio.play()
 
         },
+        getInfo() {
+            request('/right/activity/joke/get_info', {
+                user_id: uid,
+                version: version
+            }, function (res) {
+                console.log(res);
+            });
+        }
     },
     mounted() {
-
+        this.getInfo();
     },
 })
 
 
 
 
-function getAllData() {
-    request('/right/h5//wb_activity/get_all_audio', {
-        user_id: uid,
-        version: version
-    }, function (data) {
-        pageData.list = data.list || [];
-    }, function (err) {
-
-    })
-}
-
-function getData() {
-    var list = []
-    request('/right/h5//wb_activity/get_audio', {
-        user_id: uid,
-        version: version
-    }, function (data) {
-        list.push(data)
-        pageData.list = list
-    }, function (err) {})
-}
 
 if (get_url_para('sid')) {
     base64Sid = get_url_para('sid');
@@ -53,97 +41,9 @@ if (get_url_para('sid')) {
         uid = uid.split('h5')[0]
     }
     version.sid = sid;
-    pageData.uid = uid;
-    alert(sid)
 
 }
 
-
-if (get_url_para('uid')) {
-    pageData.isAll = false;
-    uid = get_url_para('uid')
-    getData()
-    pageData.uid = uid;
-} else {
-    getAllData()
-}
-
-function addAudio(url) {
-    request('/right/h5//wb_activity/add_audio', {
-        user_id: uid,
-        url: url,
-        title: pageData.name,
-        version: version
-    }, function (data) {
-        getAllData();
-        pageData.audioName = pageData.name
-        pageData.name = ''
-        showModal('.modal_share')
-        closeModal('.modal_progress')
-        document.getElementById("file").value = ''
-    }, function (err) {
-        _alert(err.msg)
-        closeModal('.modal_progress');
-        document.getElementById("file").value = ''
-    })
-}
-
-
-function upload() {
-
-    var name = pageData.name;
-    if (name.length == 0) {
-        _alert('请先输入作品名称')
-        return
-    }
-    if (name.length > 11) {
-        _alert('作品名称不能大于11个字')
-        return
-    }
-    document.getElementById('file').click()
-}
-
-
-function doUpload() {
-    if (!uid) {
-        _alert('登录后才可以参与活动哦~')
-        return;
-
-    }
-    showModal('.modal_upload')
-}
-
-
-
-function pause() {
-    pageData.playIndex = -1;
-    audio.pause();
-}
-
-function checkType(url) {
-    var fileName
-    if (url) {
-        fileName = url;
-    } else {
-        fileName = document.getElementById("file").value;
-    }
-    //返回String对象中子字符串最后出现的位置.
-    var seat = fileName.lastIndexOf(".");
-    // alert(seat)
-    if (seat < 0) {
-        return true
-    }
-    //返回位于String对象中指定位置的子字符串并转换为小写.
-    var extension = fileName.substring(seat).toLowerCase();
-    var allowed = [".weba", ".m4a", ".opus", ".mp3", '.ogg', '.wma', '.wav', '.oga', '.webm', '.mid', '.au'];
-    for (var i = 0; i < allowed.length; i++) {
-        if (extension.indexOf(allowed[i]) > -1) {
-            return true;
-        }
-    }
-    // alert("不支持"+extension+"格式");
-    return false;
-}
 
 $('body').css('visibility', 'visible');
 $(function () {
@@ -157,7 +57,7 @@ $(function () {
         },
         on: {
             slideChangeTransitionEnd: function () {
-                console.log(this.realIndex);
+                // console.log(this.realIndex);
             }
         },
     });
--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 //区分 接口地址----------开始
-var perUrl = 'http://dev-app.jycut.com';//测试环境
+var perUrl = 'http://dev-app.jycut.com'; //测试环境
 // var perUrl = 'http://115.238.40.2:16001';//测试环境
 // var perUrl = 'https://app.jycut.com';//线上环境
 // var perUrl = 'http://192.168.20.131:16001'; //开发环境 
@@ -12,7 +12,7 @@ var perUrl = 'http://dev-app.jycut.com';//测试环境
 
 
 //区分  链接地址----------开始
-var lineUrl = 'jy';//线上环境
+var lineUrl = 'jy'; //线上环境
 // var lineUrl = 'jytest';//测试环境
 // var lineUrl = 'jyoff'; //开发环境
 
