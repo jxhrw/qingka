5301df49e7993babc0be19a2550a5d1152b080b0
jxhrw <1096227957@qq.com>
2020-04-21 22:47:44 +0800

    '21最新'

--- app/activity/king/kicking.js
+++ app/activity/king/kicking.js
@@ -3,16 +3,56 @@ var pageData = new Vue({
     data: {
         num: 10,
         isMe: true,
+        pageNum: 1,
+        weekNum: 1,
     },
     methods: {
-
+        // 获取我的踢馆赛况
+        getMyChallenge: function (pageNum) {
+            request('/right/activity/joke/get_my_challenge', {
+                // user_id: uid,
+                version: version,
+                week: _this.weekNum,
+                page: pageNum,
+                page_size: 10,
+            }, function (res) {
+                console.log(res);
+            });
+        },
+        // 获取我的踢馆赛况
+        getAllChallenge: function (pageNum) {
+            request('/right/activity/joke/get_all_challenge', {
+                // user_id: uid,
+                version: version,
+                week: _this.weekNum,
+                page: pageNum,
+                page_size: 10,
+            }, function (res) {
+                console.log(res);
+            });
+        },
     },
     mounted() {
-
+        _this = this;
+        getSid();
+        this.getMyChallenge(this.pageNum);
+        this.getAllChallenge(this.pageNum);
     },
 })
 
-
+function getSid() {
+    if (get_url_para('sid')) {
+        base64Sid = get_url_para('sid');
+        var sessionId = get_url_para('sid'); //获取用户id
+        var sid = Base.decode(sessionId);
+        var sidArr = sid.split('_');
+        uid = sidArr[sidArr.length - 1];
+        if (uid.indexOf('h5') > 0) {
+            uid = uid.split('h5')[0]
+        }
+        version.sid = sid;
+    }
+}
 
 $('body').css('visibility', 'visible');
 $(function () {
